db9d99dbb3e7fb0f6f2c569dc39f1f516d8227ae
Meg Anderson <megretson@gmail.com>
2024-08-06 20:42:01 -0500

    feat(horoscope post): naieve implementation of horoscope post

---
+++ root/users/userId/horoscopes/date/POST_lambda_handler.py
@@ -0,0 +1,45 @@
+import boto3
+import json
+
+print('Loading function')
+
+client = boto3.client('dynamodb', region_name='us-east-2')
+table = 'Horoscopes'
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': json.dumps({"error": str(err)}) if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
+    access to the request and response payload, including headers and
+    status code.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+
+
+    operation = event['httpMethod']
+
+    if operation == "POST":
+        userId = event["pathParameters"]["userId"]
+        date = event["pathParameters"]["date"]
+        emojis = generateCoolEmojis()
+        horoscope = {"userId": userId, "date": date, "emojis": emojis}
+        client.client.put_item(
+            TableName = table,
+            Item = horoscope
+        )
+        return respond(None, resp)
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
