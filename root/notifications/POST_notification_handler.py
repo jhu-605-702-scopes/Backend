f296212c810f67cd3d0536eef1d4f55f620f5a95
Michael Eisemann <me@michaeleisemann.com>
2024-08-18 16:36:14 -0400

    notifications should now register

--- root/notifications/POST_notification_handler.py
+++ root/notifications/POST_notification_handler.py
@@ -4,12 +4,12 @@ import json
 print('Loading notifications function')
 
 sns = boto3.client('sns', region_name="us-east-1")
-# topic_arn = 'arn:aws:sns:us-east-1:819873747797:scopes'
+platform_application_arn = 'arn:aws:sns:us-east-1:010928195855:app/APNS_SANDBOX/Scopes-iOS'
 
 def respond(err, res=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': json.dumps({"error": str(err)}) if err else json.dumps(res),
+        'body': {"error": str(err)} if err else res,
         'headers': {
             'Content-Type': 'application/json',
         },
@@ -17,24 +17,18 @@ def respond(err, res=None):
 
 
 def post_notification_handler(event, context):
-    '''Registers a user's notification token with the SNS service'''
-    operation = event['httpMethod']
-    func_arn = context.invoked_function_arn.split(':')
-    #func_arn[3] is region, func_arn[4] is account
-    topic_arn = 'arn:aws:sns:'+func_arn[3]+':'+func_arn[4]+':scopesapp'
+    operation = event['context']['http-method']
     if operation == "POST":
-        body = json.load(event["body"])
-        if "token" not in body.keys():
-            return respond(ValueError('Missing token in body'))
-        try:
-            sns.subscribe(
-                TopicArn=topic_arn,
-                Protocol='application',
-                Endpoint=body["token"]
-            )
-            return respond(None, "Success")
-        except Exception as e:
-            return respond(e)
+        body = event["body-json"]
+        if body:
+            token = body.get("token", "")
+        if not token:
+            return respond(ValueError('Token is required'))
 
-    else:
-        return respond(ValueError('Unsupported method "{}"'.format(operation)))
+        # register device token with SNS
+        sns.create_platform_endpoint(
+            PlatformApplicationArn=platform_application_arn,
+            Token=token
+        )
+
+        return respond(None, {"message": "Device token registered successfully"})
