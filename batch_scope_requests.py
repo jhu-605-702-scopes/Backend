79507593f5e7665f8c98eb3a79b5b2fd40540893
Meg Anderson <megretson@gmail.com>
2024-08-18 18:50:43 -0500

    feat(timing): timing tests and results

--- batch_scope_requests.py
+++ batch_scope_requests.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 class BatchScopeRequests: 
 
-    def __init__(self, number_of_requests, create_user_url, create_horoscope_url) -> None:
+    def __init__(self, number_of_requests, create_user_url, create_horoscope_url, maximum_simultaneous = 10) -> None:
         self.number_of_requests = number_of_requests
         self.create_user_url = create_user_url # "https://api.getscopes.app/v1/users/"
         self.create_horoscope_url = create_horoscope_url # "https://api.getscopes.app/v1/users/{}/horoscopes/{}"
@@ -18,34 +18,36 @@ class BatchScopeRequests:
         self.date = str(datetime.today().isoformat())
         self.test_users_ids = []
         self.test_users_horoscopes = [] 
+        self.maximum_simultaneous = maximum_simultaneous
 
     # async function to make a single create user request
-    async def create_user(self, url, session):
-        async with session.post(url, ssl=False, json={"username": "DeleteThisUser"}, headers=self.headers) as response:
-            status = response.status
-            data = await response.json()
-            print(data)
-            self.test_users_ids.append(data["body"]["userId"])
+    async def create_user(self, url, session, number_to_run):
+        for i in range (number_to_run):
+            async with session.post(url, ssl=False, json={"username": "DeleteThisUser"}, headers=self.headers) as response:
+                status = response.status
+                data = await response.json()
+                self.test_users_ids.append(data["body"]["userId"])
 
     # async function to create a single user's horoscope request
-    async def create_horoscope(self, url, session):
+    async def create_horoscope(self, url, session, number_to_run):
         full_url = self.create_horoscope_url.format(self.test_users_ids.pop(), self.date)
-        async with session.post(full_url, ssl=False, json=None, headers=self.headers) as response:
-            status = response.status
-            data = await response.json()
-            emojis = json.loads(data["body"])["emojis"]
-            self.test_users_horoscopes.append(emojis)
+        for i in range (number_to_run):
+            async with session.post(full_url, ssl=False, json=None, headers=self.headers) as response:
+                status = response.status
+                data = await response.json()
+                emojis = json.loads(data["body"])["emojis"]
+                self.test_users_horoscopes.append(emojis)
 
     # async function to make multiple requests
     async def create_users(self, url):
         async with aiohttp.ClientSession() as session:
-            tasks = [self.create_user(url, session) for i in range(self.number_of_requests)]
+            tasks = [self.create_user(url, session, (self.number_of_requests // self.maximum_simultaneous)) for i in range(self.maximum_simultaneous)]
             results = await asyncio.gather(*tasks)
             return results
 
     async def create_horoscopes(self, url):
         async with aiohttp.ClientSession() as session:
-            tasks = [self.create_horoscope(url, session) for i in range(self.number_of_requests)]
+            tasks = [self.create_horoscope(url, session, (self.number_of_requests // self.maximum_simultaneous)) for i in range(self.maximum_simultaneous)]
             results = await asyncio.gather(*tasks)
             return results
 
